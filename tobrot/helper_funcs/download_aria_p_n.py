465802a41705b7e75f23ccb91d357f8ef6eba1fa
DragonPower84 <82083924+DragonPower84@users.noreply.github.com>
2021-08-04 23:28:29 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -277,7 +277,7 @@ async def call_apropriate_function(
                     message_id = final_response[key_f_res_se]
                     channel_id = str(sent_message_to_update_tg_p.chat.id)[4:]
                     private_link = f"https://t.me/c/{channel_id}/{message_id}"
-                    message_to_send += "✘ <a href='"
+                    message_to_send += "➡️ <a href='"
                     message_to_send += private_link
                     message_to_send += "'>"
                     message_to_send += local_file_name
@@ -288,7 +288,7 @@ async def call_apropriate_function(
                         f"<b><a href='tg://user?id={user_id}'>📁 Your Requested Files</a></b>\n\n"
                     )
                     message_to_send = mention_req_user + message_to_send
-                    message_to_send = message_to_send + "\n\n" + "#uploaded\n\n<b>⚡️Made By - @IamWarisAliOFFICIAL ❤️</b>"
+                    message_to_send = message_to_send + "\n\n" + "#uploads #Done"
                 else:
                     message_to_send = "<i>FAILED</i> to upload files. 😞😞"
                 await user_message.reply_text(
@@ -374,7 +374,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
             )
             await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
             await event.edit(
-                f"<b>Downloaded Successfull ✅</b>:\n\n <b>File Name</b>: \n`{file.name}`\n\n <b>Size</b>: {file.total_length_string()}"
+                f"<b>Downloaded Successfull ✅</b>:\n\n <b>File Name</b>: \n`{file.name}`\n\n <b>Size</b>: `{file.total_length_string()}`"
             )
             return True
     except aria2p.client.ClientException:

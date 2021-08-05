5bd6db9d57843fd073ecaa412c692651ddaa13aa
DragonPower84 <82083924+DragonPower84@users.noreply.github.com>
2021-08-05 17:32:46 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -54,7 +54,7 @@ async def incoming_message_f(client, message):
     user_command = message.command[0]
     g_id = message.from_user.id
     u_men = message.from_user.full_name
-    credit = await message.reply_text(f"<a href='tg://user?id={g_id}'>🧲</a> <i>Leeching For You :</i> {u_man}<b></b>", parse_mode="html")
+    credit = await message.reply_text(f"<a href='tg://user?id={g_id}'>🧲</a> <i>Leeching For You :</i> {u_men}<b></b>", parse_mode="html")
     i_m_sefg = await credit.reply_text("processing", quote=True)
     # get link from the incoming message
     if message.reply_to_message:

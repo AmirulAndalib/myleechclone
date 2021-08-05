ed30186aa62e71975e194fc61ae53cf1e54e61e9
DragonPower84 <82083924+DragonPower84@users.noreply.github.com>
2021-08-05 08:03:14 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -73,16 +73,16 @@ class Progress:
             elapsed_time = round(diff) * 1000
             time_to_completion = round((total - current) / speed) * 1000
             estimated_total_time = time_to_completion
-
+            ba_se_file_name = os.path.basename(local_file_name)
             elapsed_time = TimeFormatter(milliseconds=elapsed_time)
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress = "<b>UploadinG: `(local_file_name)` </b>\n[<b>{0}{1}</b>] -  {2}%\n".format(
+            progress = "\n[<b>{0}{1}</b>] -  {2}%\n".format(
                 ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 20))]),
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 20))]),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress + "\n<b>Uploaded : </b>{0} of {1}\n<b>Speed : </b>{2}/s\n<b>ETA : </b>{3}\n<b>Using Engine : </b>Pyrogram ".format(
+            tmp = "Uploading : " + ba_se_file_name + progress + "\n<b>Uploaded : </b>{0} of {1}\n<b>Speed : </b>{2}/s\n<b>ETA : </b>{3}\n<b>Using Engine : </b>Pyrogram ".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),

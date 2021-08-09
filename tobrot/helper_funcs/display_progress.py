a2408796ff539be9d157e7ea75ab2616f0d63988
DragonPower84 <patrobighnesh8@gmail.com>
2021-08-09 11:28:10 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -78,8 +78,15 @@ class Progress:
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
             progress = "\n[<b>{0}{1}</b>] -  {2}%\n".format(
-                ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 20))]),
-                ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 20))]),
+                "".join(
+                    [FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]
+                ),
+                "".join(
+                    [
+                        UN_FINISHED_PROGRESS_STR
+                        for i in range(20 - math.floor(percentage / 5))
+                    ]
+                ),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
             tmp = "Uploading :- " + progress + "\n<b>Uploaded : </b>{0} of {1}\n<b>Speed : </b>{2}/s\n<b>ETA : </b>{3}\n<b>Using Engine : </b>Pyrogram ".format(
